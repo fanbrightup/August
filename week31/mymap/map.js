8010873dd0d5d7d1d5d5f7a326ce37429b84f0ed
fanbrightup <fanbright@126.com>
2016-08-03 23:40:27 +0800

    高德地图的文档写的真的好,学到不少,这几日的重心就放在这里吧

--- week31/mymap/map.js
+++ week31/mymap/map.js
@@ -1,12 +1,31 @@
-// var map = new AMap.Map('container');
-// map.setZoom(10);
-// map.setCenter([116.39,39.9]);
-var smap = new AMap.Map('container',{
-        zoom: 10,
-        center: [116.39,39.9]
+//  实例化地图
+  var map = new AMap.Map('container',{
+          zoom: 10,
+          center: [116.39,39.9]
+      });
+//    实例化标记
+    var marker = new AMap.Marker({
+      position: [116.480983, 39.989628],
+      map:map
     });
-var marker = new AMap.Marker({
-        position: [116.480983, 39.989628],
-        map:smap
-    });
-    marker.setMap();
+//    实例化信息窗口
+var infowindow = new AMap.InfoWindow({
+        content: '<h3>高德地图</h1><div>高德是中国领先的数字地图内容、导航和位置服务解决方案提供商。</div>',
+        offset: new AMap.Pixel(0, -30),
+        size:new AMap.Size(230,0)
+   })
+   infowindow.setSize(new AMap.Size(150,50));
+   var contentDiv = document.createElement('div');
+   contentDiv.innerText='我是信息窗体';
+   infowindow.setContent(contentDiv);
+   var clickHandle = AMap.event.addListener(marker, 'click', function() {
+    infowindow.open(map, marker.getPosition())
+})
+
+//  添加比例尺和工具栏
+AMap.plugin(['AMap.ToolBar','AMap.Scale'],function(){
+    var toolBar = new AMap.ToolBar();
+    var scale = new AMap.Scale();
+    map.addControl(toolBar);
+    map.addControl(scale);
+})
